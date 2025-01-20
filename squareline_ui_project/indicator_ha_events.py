4d0757c196a3a8a0cbcc4e5e8767fd0aea64280c
Vittorio Boccone <drbokko@gmail.com>
2025-01-21 00:48:54 +0100

    adding ozone sensor

---
+++ squareline_ui_project/indicator_ha_events.py
@@ -0,0 +1,28 @@
+
+def switch_event_cb(event_struct):
+    return
+
+
+def switch_arc_event_cb(event_struct):
+    return
+
+
+def ui_event_wifi_config(event_struct):
+    return
+
+
+def ui_event_wifi_start(event_struct):
+    return
+
+
+def brighness_cfg_event_cb(event_struct):
+    return
+
+
+def display_cfg_apply_event_cb(event_struct):
+    return
+
+
+def mqtt_addr_changed(event_struct):
+    return
+
